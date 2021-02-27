89c3de3100869e5ff0a3adbde6b8155914b3be4f
muxto <mike.klimenko@yandex.ru>
2021-02-28 02:47:38 +0300

    measure in js, add industry

--- client/js/index.js
+++ client/js/index.js
@@ -2,85 +2,58 @@
 
 var xmlhttp = new XMLHttpRequest();
 var url = "report.json";
-let sortField = 'RecommendationTrend';
-let data;
-let zeros;
-let noneZeros;
-
-const fields = [{
-        name: 'Ticker'
-    }, {
-        name: 'Industry'
-    }, {
-        name: 'CurrentPrice'
-    }, {
-        name: 'TargetPriceHigh'
-    }, {
-        name: 'TargetPriceMean'
-    }, {
-        name: 'TargetPriceMedian'
-    }, {
-        name: 'TargetPriceLow'
-    }, {
-        name: 'StrongBuy'
-    }, {
-        name: 'Buy'
-    }, {
-        name: 'Hold'
-    }, {
-        name: 'Sell'
-    }, {
-        name: 'StrongSell'
-    },
-];
+let sortField = 'mychoice';
+let rawData;
+let stocksData;
+let model;
 
 const viewFields = [{
+        name: 'ticker',
         caption: 'Ticker',
         description: 'Ticker',
-        shortName: 't',
-        sortField: 'Ticker',
+        sortField: 't',
         sortFieldCaption: 'Ticker'
     }, {
+        name: 'industry',
         caption: 'Industry',
         description: 'Industry',
-        shortName: 'ind',
-        sortField: 'Industry',
+        sortField: 'ind',
         sortFieldCaption: 'Industry'
     }, {
+        name: 'currentPrice',
         caption: 'Current price',
         description: 'Stock current price',
-        shortName: 'c',
-        sortField: 'CurrentPrice',
+        sortField: 'c',
         sortFieldCaption: 'Current price'
     }, {
+        name: 'targetPrices',
         caption: 'Target prices',
         description: 'Low - median -  high',
-        shortName: 'price',
-        sortField: 'TargetPriceHigh',
+        sortField: 'priceh',
         sortFieldCaption: 'High target price'
     }, {
+        name: 'targetPercent',
         caption: 'Percent to current price',
         description: 'Low - median -  high',
-        shortName: 'percent',
-        sortField: 'TargetPriceHighPercent',
+        sortField: 'percenth',
         sortFieldCaption: 'High percent'
     }, {
+        name: 'rs',
         caption: 'Recommendations',
         description: 'Count of StrongBuy, Buy, Hold, Sell and StrongSell recomendations',
-        shortName: 'rts',
-        sortField: 'StrongBuy',
+        sortField: 'strongBuy',
         sortFieldCaption: 'Strong buy'
     }, {
+        name: 'rt',
         caption: 'Recommendation trend',
         description: 'Mean value of StrongBuy(1), Buy(2), Hold(3), Sell(4) and StrongSell(5)',
-        shortName: 'rt',
-        sortField: 'RecommendationTrend',
+        sortField: 'rt',
         sortFieldCaption: 'Recommendation trend'
     }, {
+        name: 'mychoice',
         caption: 'My choice',
         description: 'I would set take profit to 90% of mean target prices, when RecommendationTrend < 3',
-        shortName: 'mychoice',
-        sortField: 'MyChoice',
+        sortField: 'mychoice',
         sortFieldCaption: 'My choice percent'
     },
 ];
@@ -90,8 +63,21 @@ window.onload = function () {
     xmlhttp.send();
 };
 
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        rawData = JSON.parse(this.responseText);
+
+        updateDateRender();
+        columnDescriptionsRender();
+
+        model = getModel();
+
+        tableRender();
+    }
+};
+
 function updateDateRender() {
-    document.getElementById('updateDate').innerHTML = new Date(data.UpdateDate).toLocaleString();
+    document.getElementById('updateDate').innerHTML = new Date(rawData.UpdateDate).toLocaleString();
 }
 
 function columnDescriptionsRender() {
@@ -106,10 +92,86 @@ function columnDescriptionsRender() {
     document.getElementById('columsDescription').innerHTML = text;
 }
 
+function getModel() {
+
+    model = [];
+
+    stocksData = [...rawData.Stocks];
+
+    for (var i = 0; i < stocksData.length; i++) {
+        let row = stocksData[i];
+
+        let rowModel = {};
+
+        rowModel.t = row['Ticker'];
+        rowModel.ind = row['Industry'];
+        rowModel.c = row['CurrentPrice'];
+        rowModel.pricel = row['TargetPriceLow'];
+        rowModel.pricemean = row['TargetPriceMean'];
+        rowModel.pricemedian = row['TargetPriceMedian'];
+        rowModel.pricem = (rowModel.pricemean + rowModel.pricemedian) / 2;
+        rowModel.priceh = row['TargetPriceHigh'];
+
+        rowModel.percentl = rowModel.pricel / rowModel.c * 100 - 100;
+        rowModel.percentmean = rowModel.pricemean / rowModel.c * 100 - 100;
+        rowModel.percentmedian = rowModel.pricemedian / rowModel.c * 100 - 100;
+        rowModel.percentm = (rowModel.percentmean + rowModel.percentmedian) / 2;
+        rowModel.percenth = rowModel.priceh / rowModel.c * 100 - 100;
+
+        rowModel.strongBuy = row['StrongBuy'];
+        rowModel.buy = row['Buy'];
+        rowModel.hold = row['Hold'];
+        rowModel.sell = row['Sell'];
+        rowModel.strongSell = row['StrongSell'];
+
+        let rtn = rowModel.strongBuy +
+            rowModel.buy +
+            rowModel.hold +
+            rowModel.sell +
+            rowModel.strongSell;
+        if (rtn == 0) {
+            rowModel.rt = 0;
+        } else {
+            rowModel.rt = (rowModel.strongBuy * 1 +
+                rowModel.buy * 2 +
+                rowModel.hold * 3 +
+                rowModel.sell * 4 +
+                rowModel.strongSell * 5) / rtn;
+        }
+
+        rowModel.mychoice = getMyChoice(
+                rowModel.rt,
+                rowModel.pricel,
+                rowModel.priceh,
+                rowModel.percentmean,
+                rowModel.percentmedian);
+
+        model.push(rowModel);
+    }
+    return model;
+}
+
+function getMyChoice(rt, pricel, priceh, percentmean, percentmedian) {
+
+    if (rt === 0 || rt > 3)
+        return 0;
+
+    if (pricel === priceh)
+        return 0;
+
+    let percentm = (percentmean + percentmedian) / 2;
+
+    if (percentm < 110)
+        return 0;
+
+    let c = percentm * 0.9;
+
+    return c;
+}
+
 function tableRender() {
 
-    noneZeros = noneZeros.sort(sortByField);
-    const sortedData = noneZeros.concat(zeros);
+    model = model.sort(sortByField);
 
     let tableString = '<table><tr>';
     viewFields.forEach(({
@@ -129,112 +191,54 @@ function tableRender() {
     });
     tableString += '</tr>';
 
-    let val;
-    let t;
-    let ind;
-
-    let c;
-    let pricel;
-    let pricemean;
-    let pricemedian;
-    let pricem;
-    let priceh;
-
-    let percentl;
-    let percentmean;
-    let percentmedian;
-    let percentm;
-    let percenth;
-
-    let rt;
-    let strongBuy;
-    let buy;
-    let hold;
-    let sell;
-    let strongSell;
-
-    let mychoice;
-
-    for (var i = 0; i < sortedData.length; i++) {
+    for (var i = 0; i < model.length; i++) {
         tableString += '<tr>';
 
         viewFields.forEach(({
-                shortName
+                name
             }) => {
 
-            let row = sortedData[i];
-            t = row['Ticker'];
-            ind = row['Industry'];
-            c = row['CurrentPrice'];
-            pricel = row['TargetPriceLow'];
-            pricemean = row['TargetPriceMean'];
-            pricemedian = row['TargetPriceMedian'];
-            pricem = (pricemean + pricemedian) / 2;
-            priceh = row['TargetPriceHigh'];
+            let row = model[i];
+            let val;
 
-            percentl = pricel / c * 100 - 100;
-            percentmean = pricemean / c * 100 - 100;
-            percentmedian = pricemedian / c * 100 - 100;
-            percentm = (percentmean + percentmedian) / 2;
-            percenth = priceh / c * 100 - 100;
-
-            strongBuy = row['StrongBuy'];
-            buy = row['Buy'];
-            hold = row['Hold'];
-            sell = row['Sell'];
-            strongSell = row['StrongSell'];
-
-            let rtn = strongBuy + buy + hold + sell + strongSell;
-            if (rtn == 0) {
-                rt = 0;
-            } else {
-                rt = (strongBuy * 1 +
-                    buy * 2 +
-                    hold * 3 +
-                    sell * 4 +
-                    strongSell * 5) / rtn;
+            if (name === 'ticker') {
+                val = `<a href='https://finance.yahoo.com/quote/${row.t}'>${row.t}</a>`;
             }
-
-            mychoice = getMyChoice(rt, pricel, priceh, percentmean, percentmedian);
-
-            if (shortName === 't') {
-                val = `<a href='https://finance.yahoo.com/quote/${t}'>${t}</a>`;
-            }
-            if (shortName === 'ind') {
-                if (ind === null) {
-                    val = 'Unknown';
+            if (name === 'industry') {
+                if (row.ind === null) {
+                    val = '';
                 } else {
-                    val = ind;
+                    val = row.ind;
                 }
             }
-            if (shortName === 'c') {
-                val = c;
+            if (name === 'currentPrice') {
+                val = row.c;
             }
-            if (shortName === 'price') {
-                val = pricel;
-                if (pricel !== priceh) {
-                    val += ` - ${pricem.toFixed(2)} - ${priceh}`;
+            if (name === 'targetPrices') {
+                val = row.pricel;
+                if (row.pricel !== row.priceh) {
+                    val += ` - ${row.pricem.toFixed(2)} - ${row.priceh}`;
                 }
             }
-            if (shortName === 'percent') {
-                val = percentl.toFixed(2);
-                if (percentl !== percenth) {
-                    val += ` - ${percentm.toFixed(2)} - ${percenth.toFixed(2)}`;
+            if (name === 'targetPercent') {
+                val = row.percentl.toFixed(2);
+                if (row.percentl !== row.percenth) {
+                    val += ` - ${row.percentm.toFixed(2)} - ${row.percenth.toFixed(2)}`;
                 }
                 val = `x ${val} %`;
             }
-            if (shortName === 'rts') {
-                val = `${strongBuy}, ${buy}, ${hold}, ${sell}, ${strongSell}`;
+            if (name === 'rs') {
+                val = `${row.strongBuy}, ${row.buy}, ${row.hold}, ${row.sell}, ${row.strongSell}`;
             }
-            if (shortName === 'rt') {
-                val = rt;
+            if (name === 'rt') {
+                val = row.rt;
             }
-            if (shortName === 'mychoice') {
-                if (mychoice === 0) {
+            if (name === 'mychoice') {
+                if (row.mychoice === 0) {
                     val = 0;
                 } else {
-                    let p1 = pricem * 0.9;
-                    let p2 = percentm * 0.9;
+                    let p1 = row.pricem * 0.9;
+                    let p2 = row.percentm * 0.9;
                     val = `${p1.toFixed(2)} x ${p2.toFixed(2)} %`;
                 }
             }
@@ -267,9 +271,9 @@ function sortByField({
 }, {
     [sortField]: b
 }) {
-    if (sortField === 'Ticker' ||
-        sortField === 'Industry' ||
-        sortField === 'RecommendationTrend') {
+    if (sortField === 't' ||
+        sortField === 'ind' ||
+        sortField === 'rt') {
         return compare(a, b);
     } else {
         return compare(b, a);
@@ -284,39 +288,4 @@ function addListeners() {
             tableRender();
         });
     });
-};
-
-function getMyChoice(rt, pricel, priceh, percentmean, percentmedian) {
-
-    if (rt === 0 || rt > 3)
-        return 0;
-
-    if (pricel === priceh)
-        return 0;
-
-    let percentm = (percentmean + percentmedian) / 2;
-
-    if (percentm < 110)
-        return 0;
-
-    let c = percentm * 0.9;
-
-    return c;
-}
-
-xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        data = JSON.parse(this.responseText);
-
-        updateDateRender();
-        columnDescriptionsRender();
-
-        const stocksData = [...data.Stocks];
-
-        zeros = stocksData.filter((item) => item.RecommendationTrend === 0);
-        noneZeros = stocksData.filter((item) => item.RecommendationTrend !== 0);
-
-        tableRender();
-    }
-
 };
